a253bf01f08e580cd7e4cdd37af16991e9e963ce
yuta.onishi <rad6242@icloud.com>
2022-12-04 16:10:31 +0900

    Notion::Databaseをmigrate

--- db/migrate/20221204065908_create_notion_databases.rb
+++ db/migrate/20221204065908_create_notion_databases.rb
@@ -1,7 +1,9 @@
 class CreateNotionDatabases < ActiveRecord::Migration[7.0]
   def change
     create_table :notion_databases do |t|
-
+      t.string :name
+      t.text :token
+      t.text :url
       t.timestamps
     end
   end
